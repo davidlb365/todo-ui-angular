ffdc5b81757223558055dc69faf12bd6381c71ad
Davidlb365 <davidlb365@gmail.com>
2025-04-21 11:49:00 +0200

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  baseUrl: 'http://localhost:8080/api',
+  // baseUrl: 'http://localhost:8080/api',
+  // baseUrl: 'http://localhost:3000/api',
+  baseUrl: 'https://todo-node-eta.vercel.app/api',
 };
